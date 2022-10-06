9f74947c0a3cfda6c9119e105d49db410e6af620
Oskar Jorgensen <oskarjorgensen@gmail.com>
2022-10-07 00:10:06 +0200

    comment utils.py

--- utils.py
+++ utils.py
@@ -1,35 +1,124 @@
-from typing import Tuple
 import pandas as pd
-import numpy as np
-import seaborn as sns
-import sklearn
-import matplotlib.pyplot as plt
 
-def group_df(dataframe: pd.DataFrame, column_to_group_by: Tuple[str]) -> pd.DataFrame:
+def group_df(dataframe: pd.DataFrame, column_to_group_by: str) -> pd.DataFrame:
+    """_summary_
+
+    Args:
+        dataframe (pd.DataFrame): The dataframe to be processed.
+        column_to_group_by (str): The column we group the dataframe by. Should take 
+        "fylke", "kommune" or "delomrade" as value.
+
+    Returns:
+        pd.DataFrame: The dataframe grouped by specified column.
+    """
     new_df = dataframe.copy()
     new_df = new_df.groupby([column_to_group_by]).sum()
     return new_df
 
-def drop_oldest_duplicates(dataframe: pd.DataFrame, unique_column_name: str, year_column_name: str = "year") -> pd.DataFrame:
+def drop_oldest_duplicates(
+    dataframe: pd.DataFrame, 
+    grunnkrets_column_name: str = "grunnkrets_id", 
+    year_column_name: str = "year"
+    ) -> pd.DataFrame:
+    """Only retains the newest information available for each grunnkrets id, and removes old information.
+
+    Args:
+        dataframe (pd.DataFrame): The dataframe to be processed.
+        grunnkrets_column_name (str, optional): The name of the column containing the 
+                                                "grunnkrets" information. Defaults to "grunnkrets_id".
+        year_column_name (str, optional): The name of the column containing the "year" information. 
+                                          Defaults to "year".
+
+    Returns:
+        pd.DataFrame: The new dataframe containing only the newest data, without duplicates.
+    """
     new_df = dataframe.copy()
     new_df = new_df.sort_values(by=[year_column_name])
-    new_df = new_df.drop_duplicates(unique_column_name, keep="last")
+    new_df = new_df.drop_duplicates(grunnkrets_column_name, keep="last")
     return new_df
 
 def create_fylke_column(dataframe: pd.DataFrame, grunnkrets_column_name: str = "grunnkrets_id") -> pd.DataFrame:
+    """Decodes the grunnkrets_id column into 1 new columns for "fylke" information.
+
+    Args:
+        dataframe (pd.DataFrame): The dataframe to be processed.
+        grunnkrets_column_name (str, optional): The name of the column containing the 
+                                                "grunnkrets" information. Defaults to "grunnkrets_id".
+
+    Returns:
+        pd.DataFrame: _description_
+    """
     dataframe["fylke"] = dataframe[grunnkrets_column_name].apply(lambda x: int(str(x)[:len(str(x)) - 6]))
     return dataframe
 
 def create_kommune_column(dataframe: pd.DataFrame, grunnkrets_column_name: str = "grunnkrets_id") -> pd.DataFrame:
+    """Decodes the grunnkrets_id column into 1 new columns for "kommune" information.
+
+    Args:
+        dataframe (pd.DataFrame): The dataframe to be processed.
+        grunnkrets_column_name (str, optional): The name of the column containing the 
+                                                "grunnkrets" information. Defaults to "grunnkrets_id".
+
+    Returns:
+        pd.DataFrame: _description_
+    """
     dataframe["kommune"] = dataframe[grunnkrets_column_name].apply(lambda x: int(str(x)[:len(str(x)) - 4]))
     return dataframe
 
 def create_delomrade_column(dataframe: pd.DataFrame, grunnkrets_column_name: str = "grunnkrets_id") -> pd.DataFrame:
+    """Decodes the grunnkrets_id column into 1 new columns for "delomrade" information.
+
+    Args:
+        dataframe (pd.DataFrame): The dataframe to be processed.
+        grunnkrets_column_name (str, optional): The name of the column containing the 
+                                                "grunnkrets" information. Defaults to "grunnkrets_id".
+
+    Returns:
+        pd.DataFrame: _description_
+    """
     dataframe["delomrade"] = dataframe[grunnkrets_column_name].apply(lambda x: int(str(x)[:len(str(x)) - 2]))
     return dataframe
 
 def create_geographical_columns(dataframe: pd.DataFrame, grunnkrets_column_name: str = "grunnkrets_id") -> pd.DataFrame:
+    """Decodes the grunnkrets_id column into 3 new columns. A column for "fylke",
+    a column for "kommune" and a column for "delomrade".
+
+    Args:
+        dataframe (pd.DataFrame): The dataframe to be processed.
+        grunnkrets_column_name (str, optional): The name of the column containing the 
+                                                "grunnkrets" information. Defaults to "grunnkrets_id".
+
+    Returns:
+        pd.DataFrame: The modified dataframe.
+    """
     dataframe = create_fylke_column(dataframe=dataframe, grunnkrets_column_name=grunnkrets_column_name)
-    dataframe = create_delomrade_column(dataframe=dataframe, grunnkrets_column_name=grunnkrets_column_name)
     dataframe = create_kommune_column(dataframe=dataframe, grunnkrets_column_name=grunnkrets_column_name)
+    dataframe = create_delomrade_column(dataframe=dataframe, grunnkrets_column_name=grunnkrets_column_name)
+    return dataframe
+
+def preprocess_grunnkrets_df(
+    dataframe: pd.DataFrame, 
+    grunnkrets_column_name: str = "grunnkrets_id", 
+    year_column_name: str = "year"
+    ) -> pd.DataFrame:
+    """This takes in a pandas dataframe containing data related to grunnkrets_ids
+    and preprocesses it. Only keeps newest information available for each grunnkrets
+    and decodes the grunnkrets_id into "fylke", "kommune" and "delomrade".
+
+    Args:
+        dataframe (pd.DataFrame): The dataframe to be preprocessed
+        grunnkrets_column_name (str, optional): The name of the column containing the 
+                                                "grunnkrets" information. Defaults to "grunnkrets_id".
+        year_column_name (str, optional): The name of the column containing the "year" information. 
+                                          Defaults to "year".
+
+    Returns:
+        pd.DataFrame: The modified dataframe
+    """
+    dataframe = drop_oldest_duplicates(
+        dataframe=dataframe, 
+        grunnkrets_column_name=grunnkrets_column_name, 
+        year_column_name=year_column_name
+        )
+    dataframe = create_geographical_columns(dataframe=dataframe, grunnkrets_column_name=grunnkrets_column_name)
     return dataframe
