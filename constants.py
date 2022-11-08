2dc21508cb34aad4ec2d110e32a377c833ff64dd
Halvor Linder <halvor_lh@hotmail.no>
2022-11-08 11:12:05 +0100

    add number of stores in geographical units

--- constants.py
+++ constants.py
@@ -1,2 +1,3 @@
 levels = ['grunnkrets_id', 'delomrade', 'kommune', 'fylke']
 income_cols = ['all_households', 'singles', 'couple_without_children',	'couple_with_children', 'other_households',	'single_parent_with_children']
+plaace_cols = ['plaace_cat_1',	'plaace_cat_2',	'plaace_cat_3',	'plaace_cat_4']
