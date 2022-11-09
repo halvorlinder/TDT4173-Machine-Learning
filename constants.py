2194a5b2df7d74900358d35b98f9ec43c43bb798
Halvor Linder <halvor_lh@hotmail.no>
2022-11-09 11:04:51 +0100

    add constant

--- constants.py
+++ constants.py
@@ -1,3 +1,4 @@
 levels = ['grunnkrets_id', 'delomrade', 'kommune', 'fylke']
+next_nevel = {'grunnkrets_id':'delomrade', 'delomrade':'kommune', 'kommune':'fylke'}
 income_cols = ['all_households', 'singles', 'couple_without_children',	'couple_with_children', 'other_households',	'single_parent_with_children']
 plaace_cols = ['plaace_cat_1',	'plaace_cat_2',	'plaace_cat_3',	'plaace_cat_4']
