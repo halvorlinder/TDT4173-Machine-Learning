67f40440005ca88b3248068e725f735b721e8142
Halvor Linder <halvor_lh@hotmail.no>
2022-11-09 11:54:33 +0100

    add more plaace to mean income

--- avg_revenue.py
+++ avg_revenue.py
@@ -5,24 +5,33 @@ from constants import *
 
 def add_avg_revenue(df: pd.DataFrame, total: bool=False):
     result_df = split_plaace_cat(create_geographical_columns(df))
-    for level in levels:
-        grouped_df= pd.read_csv(f'temp_data/{level}_mean_revenue{"_total" if total else ""}.csv', dtype={'plaace_cat_1':object})
-        result_df = pd.merge(result_df, grouped_df, on=[level, 'plaace_cat_1'], how='left',suffixes=('', '_redundant'))
-    for level in levels[::-1][1::]:
-        result_df[f'{level}.mean_revenue'].fillna(result_df[f'{next_nevel[level]}.mean_revenue'], inplace=True) 
+    result_df['country'] = 1
+    for level in levels_ext:
+        for cat in plaace_cols:
+            grouped_df= pd.read_csv(f'temp_data/{level}_{cat}_mean_revenue{"_total" if total else ""}.csv', dtype={cat:object})
+            result_df = pd.merge(result_df, grouped_df, on=[level, cat], how='left',suffixes=('', '_redundant'))
+
+    # Impute nans 
+    for level in levels[::-1]:
+        for cat in plaace_cols:
+            result_df[f'{level}.{cat}_mean_revenue'].fillna(result_df[f'{next_nevel_ext[level]}.{cat}_mean_revenue'], inplace=True) 
+    for level in levels_ext:
+        for cat in plaace_cols[1:]:
+            result_df[f'{level}.{cat}_mean_revenue'].fillna(result_df[f'{level}.{next_plaace_col[cat]}_mean_revenue'], inplace=True) 
     return result_df
     
 def create_avg_revenue_csvs():
     stores_total_train = pd.read_csv("data/stores_train.csv")
     stores_train, stores_val = train_test_split(stores_total_train, test_size=0.2, random_state=0)
     revenue_df = split_plaace_cat(create_geographical_columns(stores_train))
-    revenue_df['count'] = 1
+    revenue_df['country'] = 1
     revenue_df_total = split_plaace_cat(create_geographical_columns(stores_total_train))
-    revenue_df_total['count'] = 1
-    for level in levels:
-        d1 = {'revenue':f'{level}.mean_revenue', 'count':f'{level}.mean_revenue_count'}
-        d2 = {'revenue':f'{level}.mean_revenue', 'count':f'{level}.mean_revenue_count'}
-        grouped_df = revenue_df.groupby([level, 'plaace_cat_1']).agg( {'revenue':'mean', 'count':'sum'} ).rename(columns=d1)
-        grouped_df_total = revenue_df_total.groupby([level, 'plaace_cat_1']).agg( {'revenue':'mean', 'count':'sum'} ).rename(columns=d2)
-        grouped_df.to_csv(f'temp_data/{level}_mean_revenue.csv')
-        grouped_df_total.to_csv(f'temp_data/{level}_mean_revenue_total.csv')
+    revenue_df_total['country'] = 1
+    for level in levels_ext:
+        for cat in plaace_cols:
+            d1 = {'revenue':f'{level}.{cat}_mean_revenue'}
+            d2 = {'revenue':f'{level}.{cat}_mean_revenue'}
+            grouped_df = revenue_df.groupby([level, cat]).agg( {'revenue':'mean'} ).rename(columns=d1)
+            grouped_df_total = revenue_df_total.groupby([level, cat]).agg( {'revenue':'mean'} ).rename(columns=d2)
+            grouped_df.to_csv(f'temp_data/{level}_{cat}_mean_revenue.csv')
+            grouped_df_total.to_csv(f'temp_data/{level}_{cat}_mean_revenue_total.csv')
