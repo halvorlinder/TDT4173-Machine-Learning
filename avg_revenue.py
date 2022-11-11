034714e9b2be90d4db8489347ac7aaa9899ae254
Halvor Linder <halvor_lh@hotmail.no>
2022-11-11 17:02:34 +0100

    add plaace_cat_0

--- avg_revenue.py
+++ avg_revenue.py
@@ -6,14 +6,15 @@ from constants import *
 def add_avg_revenue(df: pd.DataFrame, total: bool=False):
     result_df = split_plaace_cat(create_geographical_columns(df))
     result_df['country'] = 1
+    result_df['plaace_cat_0'] = '1'
     for level in levels_ext:
-        for cat in plaace_cols:
-            grouped_df= pd.read_csv(f'temp_data/{level}_{cat}_mean_revenue{"_total" if total else ""}.csv', dtype={cat:object})
+        for cat in plaace_cols_ext:
+            grouped_df = pd.read_csv(f'temp_data/{level}_{cat}_mean_revenue{"_total" if total else ""}.csv', dtype={cat:object})
             result_df = pd.merge(result_df, grouped_df, on=[level, cat], how='left',suffixes=('', '_redundant'))
 
     # Impute nans 
     for level in levels[::-1]:
-        for cat in plaace_cols:
+        for cat in plaace_cols_ext:
             result_df[f'{level}.{cat}_mean_revenue'].fillna(result_df[f'{next_nevel_ext[level]}.{cat}_mean_revenue'], inplace=True) 
     for level in levels_ext:
         for cat in plaace_cols[1:]:
@@ -25,10 +26,12 @@ def create_avg_revenue_csvs():
     stores_train, stores_val = train_test_split(stores_total_train, test_size=0.2, random_state=0)
     revenue_df = split_plaace_cat(create_geographical_columns(stores_train))
     revenue_df['country'] = 1
+    revenue_df['plaace_cat_0'] = '1'
     revenue_df_total = split_plaace_cat(create_geographical_columns(stores_total_train))
     revenue_df_total['country'] = 1
+    revenue_df_total['plaace_cat_0'] = '1'
     for level in levels_ext:
-        for cat in plaace_cols:
+        for cat in plaace_cols_ext:
             d1 = {'revenue':f'{level}.{cat}_mean_revenue'}
             d2 = {'revenue':f'{level}.{cat}_mean_revenue'}
             grouped_df = revenue_df.groupby([level, cat]).agg( {'revenue':'mean'} ).rename(columns=d1)
